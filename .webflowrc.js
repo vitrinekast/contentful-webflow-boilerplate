54d1959257b2ad7cd891d165e6f3354246eef5b1
vitrinekast <roosschuurmans@gmail.com>
2024-04-30 17:22:28 +0200

    setup starter

---
+++ .webflowrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "https://api.webflow.com",
+    rootDir: "./devlink",
+    siteId: process.env.WF_SITE_ID,
+    authToken: process.env.WF_AUTH_TOKEN,
+    cssModules: false
+}
